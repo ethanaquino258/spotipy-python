d83a52f2e01ddacad94b5a6cef90cca1d556a9dd
Ethan Aquino <ethan.aquino.258@gmail.com>
2021-04-05 22:36:10 -0500

    got artist playlist w rng done, but it sometimese grabs another artist's tracks for some reason. Need to investigate

--- src/artists.py
+++ src/artists.py
@@ -1,8 +1,9 @@
 from authentication import authCode
 import spotipy
+import random
 
 def topArtists():
-    client = authCode("user-top-read")
+    client = authCode("user-top-read playlist-modify-public")
 
     timePicker = input("""Please choose a time range 
         1. short-term
@@ -24,16 +25,44 @@ def topArtists():
 
     print("the popularity metric ranges from 0-100, w/ 100 as the highest level of popularity attainable")
     
+    uriList =[]
+
     for idx, item in enumerate(artists):
         artistObject = artists[idx]
-
+        
         artist = artistObject['name']
         genres = artistObject['genres']
         followers = artistObject['followers']['total']
         popularity = artistObject['popularity']
-
-
+    
         print(f'{idx + 1}. {artist}\nGenres: {genres}\nFollowers: {followers}\nPopularity: {popularity}\n')
+        
+        uriList.append(item['uri'])
+    
+    createOption = input("Would you like to make a playlist? y/n\nNOTE: one random track from a random album in an artist's discography will be added per artist")
+
+    if createOption == "y":
+        playlistName = input("please enter a name for the playlist:\n")
+
+        user = client.me()
+        createResults = client.user_playlist_create(user['id'], playlistName)
+
+        newPlaylistID = createResults['id']
+
+        addTracks = []
+        for artist in uriList:
+            albumResults = client.artist_albums(artist,limit=50)
+            albumList = albumResults['items']
+            index = random.randint(0, (len(albumList)-1))
+            chosenAlbum = albumList[index]['uri']
+            albumTracks = client.album_tracks(chosenAlbum, limit=50)
+            trackList = albumTracks['items']
+            index2 = random.randint(0, (len(trackList)-1))
+            chosenTrack = trackList[index2]['uri']
+            addTracks.append(chosenTrack)
+
+        client.user_playlist_add_tracks(user['id'], newPlaylistID, addTracks)
+
     exit()
 
 def followedArtists():
